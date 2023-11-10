960874dafd52f83e8a3729ce13452838c01335db
Natalie Burgei <natalieburgei04@gmail.com>
2023-11-10 16:30:18 +0300

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
-
+import React from "react";
 function Keypad (){
+    function changeHandler(e){
+        e.preventDefault()
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={changeHandler} type="password" />
+        </div>
     )
 }
 
